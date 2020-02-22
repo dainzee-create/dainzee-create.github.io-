198a0bea5c8762570ed53e5f211d8f13d895e567
dainzee-create <dainzee@byui.edu>
2020-02-21 18:23:53 -0700

    '

--- lesson07/lazy.js
+++ lesson07/lazy.js
@@ -1,29 +1,29 @@
-const imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad =document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
   threshold: 1,
-  rootMargin: "0px 0px 50px"
+  rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+  image.onload = () => {image.removeAttribute('data-src');};
 };
 
-if('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items, observer) => {
+if ('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
       if(item.isIntersecting) {
         loadImages(item.target);
-        observer.unobserve(item.target);
+        imgObserver.unobserve(item.target);
       }
     });
-  });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
+}, imgOptions);
+
+imagesToLoad.forEach((img) => {
+  imgObserver.observe(img)
+});
+
 } else {
   imagesToLoad.forEach((img) => {
     loadImages(img);
