6f084e386ccaf6ee61aed4fe81b6bd82ce649d61
dainzee-create <dainzee@byui.edu>
2020-03-17 11:37:57 -0600

    api

--- lesson10/js/preston-api.js
+++ lesson10/js/preston-api.js
@@ -1,24 +1,23 @@
-const apiURL2 = `https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=d068bc952bd45492d2ab0b69e8411d2c`
+const apiURL2 = `https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=d068bc952bd45492d2ab0b69e8411d2c`;
 
 
 fetch(apiURL2)
 .then((response) => response.json())
 .then((jsObject) => {
-  console.log(jsObject);
 
-    fiveDay = [];
-    for (let i = 0; i <= jsObject.list.length < 6; i++ ) {
-    if(jsObject.list[i].dt_text.includes("18:00:00")) {
-        fiveDay[i] = jsObject.list[i].main.temp;
+    let dayOption = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let days = 1;
+    for (let day of jsObject.list) {
+      if (day.dt_txt.includes("18:00:00")) {
+        let date = new Date(day.dt_txt);
+        let dayId = "forecast" + days;
+        const iconImage = `https://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+        document.getElementById(dayId).textContent = dayOption[date.getDay()];
+        document.getElementById(`${dayId}_temp`).textContent = day.main.temp.toFixed(0) + " F";
+        document.getElementById(`${dayId}_image`).setAttribute("src", iconImage);
+        document.getElementById(`${dayId}_image`).setAttribute("alt", day.weather[0].description);
+        days++;
       }
-
-      document.getElementById("forecast1") = forecast[i].main.temp;
-      document.getElementById("forecast2") = forecast[i].main.temp;
-      document.getElementById("forecast3") = forecast[i].main.temp;
-      document.getElementById("forecast4") = forecast[i].main.temp;
-      document.getElementById("forecast5") = forecast[i].main.temp;
-
     }
   });
-
 
