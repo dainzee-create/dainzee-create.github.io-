b4c00b9385721902764be21debb64b62121ac48d
dainzee-create <dainzee@byui.edu>
2020-04-07 10:04:32 -0600

    m

--- L13 Final/js/currentdate.js
+++ L13 Final/js/currentdate.js
@@ -1,14 +1,5 @@
-var date = new Date(),
-  weekDay = date.getUTCDay(),
-  year = date.getFullYear(),
-  month = date.getMonth(),
-  day = date.getUTCDate(),
-  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-document.getElementById("currentdate").innerHTML = weekday[weekDay] + ", " + day + " " + months[month] + " " + year;
-
-const options = {
-  year: "numeric"
-};
-document.getElementById("currentyear").textContent = new Date().toLocaleDateString("en-US", options);
+function getYear() {
+var d = new Date();
+var y = d.getFullYear();
+document.getElementById("currentyear").innerHTML = y;
+}
