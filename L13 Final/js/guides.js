73a084f5125704e1489dfb6f044ca3f22f594e58
dainzee-create <dainzee@byui.edu>
2020-04-08 10:50:49 -0600

    o

--- L13 Final/js/guides.js
+++ L13 Final/js/guides.js
@@ -1,36 +1,34 @@
-const src="guide.json"
+  const guides = "L13 Final\guide.json";
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);
-
-    const prophets = jsonObject['prophets'];
-
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < guides.length; i++ ) {
 
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let date = document.createElement('p');
-      let place = document.createElement('p');
-      let image = document.createElement('img');
+      let certification = document.createElement('p');
+      let years = document.createElement('p');
+      let email = document.createElement('p');
+      let bio = document.createElement('p');
+      let imageurl = document.createElement('img');
 
-      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      date.textContent = "Date of Birth: " + prophets[i].birthdate;
-      place.textContent = "Place of Birth: " + prophets[i].birthplace;
+      h2.textContent = guides[i].name + ' ' + guides[i].lastname;
+      certification.textContent = "Certification Level: " + guides[i].certification;
+      years.textContent = "Years of Experience: " + guides[i].years;
+      email.textContent = "Email: " + guides[i].email;
+      bio.textContent = "Bio: " + guides[i].bio;
 
-      image.setAttribute('src', prophets[i].imageurl);
-      image.setAttribute('alt', "Picture of: " + h2.textContent + "--" + prophets[i].order);
+
+      imageurl.setAttribute('src', guides[i].imageurl);
+      imageurl.setAttribute('alt', "Picture of: " + h2.textContent);
 
 
       card.appendChild(h2);
-      card.appendChild(date);
-      card.appendChild(place);
-      card.appendChild(image);
+      card.appendChild(certification);
+      card.appendChild(years);
+      card.appendChild(email);
+      card.appendChild(bio);
+      card.appendChild(imageurl);
 
-      document.querySelector('div.cards').appendChild(card);
-  }});
+      document.querySelector('div.guides').appendChild(card);
+  };
 
 
